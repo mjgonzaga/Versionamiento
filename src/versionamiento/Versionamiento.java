449ef1fb60185e712c1814b293f93a6ed5c10b9b
mjgonzaga <mgonzaga@utpl.edu.ec>
2014-06-05 23:22:46 -0500

    Creacion de instancia de la clase DatosPersonales

--- src/versionamiento/Versionamiento.java
+++ src/versionamiento/Versionamiento.java
@@ -16,7 +16,27 @@ public class Versionamiento {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Se crea un objento de los datos personales
+        DatosPersonales persona1 = new DatosPersonales();
+                //Ingreso los datos con los Setter a la nueva instancia de datos personales
+                persona1.setNombre("Marianela");
+                persona1.setApellido("Gonzaga");
+                persona1.setCedula("1104679541");
+                persona1.setMail("mgonzaga@utpl.edu.ec");
+                persona1.setDireccion("Miraflores bajo");
+                persona1.setTelefono("0986740629");
+                persona1.setFecha_nac("23 de Junio de 1992");
+                persona1.setEdad(21);
+
+                //Para obtener los datos uso los Getter
+                System.out.println(persona1.getNombre());
+                System.out.println(persona1.getApellido());
+                System.out.println(persona1.getCedula());
+                System.out.println(persona1.getMail());
+                System.out.println(persona1.getDireccion());
+                System.out.println(persona1.getTelefono());
+                System.out.println(persona1.getFecha_nac());
+                System.out.println(persona1.getEdad());
     }
     
 }
